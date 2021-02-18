8a5d9c3ca41ffe4015ae775b3b9e03d20c675220
Afri <79155979+Afrii03@users.noreply.github.com>
2021-02-18 16:07:06 +0100

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,34 @@
+  import java.util.Scanner;
+
+public class App {
+  public static void main(String[] args) throws Exception {  
+    Scanner scanner = new Scanner(System.in);
+   
+    String[][] dane = {
+      {"Więcej niż jedno zwierzę","stado","klucz","dwa","owca","lama"},
+      {"Sporty na s","sztafeta","skok o tyczce", "skoki narciarskie", "sumo", "szachy"}
+    };
+
+    int wynik = 0;
+
+    for(int i = 0; i < dane.length; i++){
+      System.out.println(dane[i][0]);
+      String odp = scanner.nextLine(); 
+      boolean isAnswerCorrect = false;
+      
+      for(int it = 1; it < dane[i].length; it++){
+        
+        if(dane[i][it].equals(odp)){
+          isAnswerCorrect = true;
+          System.out.println( "BRZDĘK" );
+          wynik += it*10;
+        }
+      }
+      if(!isAnswerCorrect){
+        System.out.println("X");
+      }
+    }      
+    System.out.println("Twój wynik to "+wynik);
+    scanner.close();
+  }
+}
